b205c8ff56cf8294818b0e12578fcc676689f514
namp8 <namp8@hotmail.com>
2019-07-25 10:24:17 +0000

    backups

---
+++ database backups/ups20190723.sql
@@ -0,0 +1,5 @@
+SELECT `stock_materials`.`stock_material_id`,
+    `stock_materials`.`material_id`,
+    `stock_materials`.`machine_id`,
+    `stock_materials`.`bags`
+FROM `ups_db`.`stock_materials`;
